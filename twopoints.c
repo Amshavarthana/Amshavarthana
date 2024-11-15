bab8a1c63837c1f8490abe8f452e6992dcc94b00
Amshavarthana <amshavarthana42@gmail.com>
2024-11-15 12:27:34 +0530

    two points

---
+++ twopoints.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float A=2.12,B=3.88,product;
+    product=A*B;
+  //displaying results up to 3 decimal places  
+    printf("product of the entered numbered is :%3f", product);
+    
+}
