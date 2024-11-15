98c81a7991d813e28f0a852fc9cdc916278561a9
Amshavarthana <amshavarthana42@gmail.com>
2024-11-15 12:26:38 +0530

    size of header

---
+++ sizeofheader.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    //determine and print sizeof int
+    printf("size of int:%u bytes\n",sizeof(int));
+    
+    //determine and print size of float
+    printf("size of float:%u bytes\n",sizeof(float));
+    
+    //determine and print size of double
+    printf("size of double:%u bytes\n",sizeof(double));
+    
+    //determine and print sizeof char
+    printf("size of char:%u bytes\n",sizeof(char));
+}
+
